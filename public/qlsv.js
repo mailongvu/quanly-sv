7388b65fb28547710fa41a8d9bda486ca5c9913a
VuCodiing <vucodiing@VuCodiings-iMac.local>
2020-09-08 21:39:18 +0700

    update find

--- public/qlsv.js
+++ public/qlsv.js
@@ -1,47 +1,47 @@
-let http = "https://vucodiing-students.herokuapp.com/users";
+let http =
+  "https://vucodiing-students.herokuapp.com/users?_sort=id&_order=desc";
 let studentID = 0;
+let getInfo = $("#find-smt").val();
 $(function () {
-
   $.ajax({
-    type: "GET",
-    url: http + "?_sort=id &_order=desc &_page=1",
-    
-    context: document.body
+    method: "GET",
+    url: http + "&_page=1",
+
+    context: document.body,
   }).done(function (users) {
     let htmlString = "";
 
     for (let user of users) {
       htmlString += `<tr id="${user.id}">
-        <td>${user.name}</td>
-        <td>${user.birthday}</td>
-        <td>${user.email}</td>
-        <td>${user.phone}</td>
-        <td>
-            <a href="repair-student.html?id=${user.id}" class="text-repair">
-                <i class="fa fa-edit">
-                </i>
-                Chỉnh sửa
-            </a>
-            |
-            <a href="javascript:void(0)" onclick="confirmDel(${user.id})" class="text-delete" data-toggle="modal" data-target="#exampleModal">
-
-                <i class="fa fa-trash-alt">
-                </i>
-                Xoá
-            </a>
-        </td>
-    </tr>`
+          <td>${user.name}</td>
+          <td>${user.birthday}</td>
+          <td>${user.email}</td>
+          <td>${user.phone}</td>
+          <td>
+              <a href="repair-student.html?id=${user.id}" class="text-repair">
+                  <i class="fa fa-edit">
+                  </i>
+                  Chỉnh sửa
+              </a>
+              |
+              <a href="javascript:void(0)" onclick="confirmDel(${user.id})" class="text-delete" data-toggle="modal" data-target="#exampleModal">
+  
+                  <i class="fa fa-trash-alt">
+                  </i>
+                  Xoá
+              </a>
+          </td>
+      </tr>`;
     }
     $("tbody").html(htmlString);
   });
-
-})
+});
 
 function btn1() {
   $.ajax({
-    type: "GET",
-    url: http + "?_sort=id &_order=desc &_page=1",
-    context: document.body
+    method: "GET",
+    url: http + "&_page=1",
+    context: document.body,
   }).done(function (users) {
     let htmlString = "";
 
@@ -65,7 +65,7 @@ function btn1() {
                 Xoá
             </a>
         </td>
-    </tr>`
+    </tr>`;
     }
     $("tbody").html(htmlString);
   });
@@ -73,9 +73,9 @@ function btn1() {
 
 function btn2() {
   $.ajax({
-    type: "GET",
-    url: http + "?_sort=id &_order=desc &_page=2",
-    context: document.body
+    method: "GET",
+    url: http + "&_page=2",
+    context: document.body,
   }).done(function (users) {
     let htmlString = "";
 
@@ -99,7 +99,7 @@ function btn2() {
                 Xoá
             </a>
         </td>
-    </tr>`
+    </tr>`;
     }
     $("tbody").html(htmlString);
   });
@@ -107,9 +107,9 @@ function btn2() {
 
 function btn3() {
   $.ajax({
-    type: "GET",
-    url: http + "?_sort=id &_order=desc &_page=3",
-    context: document.body
+    method: "GET",
+    url: http + "&_page=3",
+    context: document.body,
   }).done(function (users) {
     let htmlString = "";
 
@@ -133,7 +133,7 @@ function btn3() {
                 Xoá
             </a>
         </td>
-    </tr>`
+    </tr>`;
     }
     $("tbody").html(htmlString);
   });
@@ -141,9 +141,9 @@ function btn3() {
 
 function btn4() {
   $.ajax({
-    type: "GET",
-    url: http + "?_sort=id &_order=desc &_page=4",
-    context: document.body
+    method: "GET",
+    url: http + "&_page=4",
+    context: document.body,
   }).done(function (users) {
     let htmlString = "";
 
@@ -167,10 +167,115 @@ function btn4() {
                 Xoá
             </a>
         </td>
-    </tr>`
+    </tr>`;
     }
     $("tbody").html(htmlString);
   });
+}
+
+function filterName() {
+  if ($("#find-smt").val() === "") {
+    alert("Nhập từ khoá tìm kiếm");
+  } else {
+    let nameFilter = "name=" + $("#find-smt").val();
+    let birthdayFilter = "birthday=" + $("#find-smt").val();
+    let phoneFilter = "phone=" + $("#find-smt").val();
+    $.ajax({
+      method: "GET",
+      url: "https://vucodiing-students.herokuapp.com/users/?" + nameFilter,
+      context: document.body,
+    }).done(function (users) {
+      let htmlString = "";
+
+      for (let user of users) {
+        htmlString += `<tr id="${user.id}">
+          <td>${user.name}</td>
+          <td>${user.birthday}</td>
+          <td>${user.email}</td>
+          <td>${user.phone}</td>
+          <td>
+              <a href="repair-student.html?id=${user.id}" class="text-repair">
+                  <i class="fa fa-edit">
+                  </i>
+                  Chỉnh sửa
+              </a>
+              |
+              <a href="javascript:void(0)" onclick="confirmDel(${user.id})" class="text-delete" data-toggle="modal" data-target="#exampleModal">
+  
+                  <i class="fa fa-trash-alt">
+                  </i>
+                  Xoá
+              </a>
+          </td>
+      </tr>`;
+      }
+      $("tbody").html(htmlString);
+    });
+
+    $.ajax({
+      method: "GET",
+      url: "https://vucodiing-students.herokuapp.com/users/?" + birthdayFilter,
+      context: document.body,
+    }).done(function (users) {
+      let htmlString = "";
+
+      for (let user of users) {
+        htmlString += `<tr id="${user.id}">
+          <td>${user.name}</td>
+          <td>${user.birthday}</td>
+          <td>${user.email}</td>
+          <td>${user.phone}</td>
+          <td>
+              <a href="repair-student.html?id=${user.id}" class="text-repair">
+                  <i class="fa fa-edit">
+                  </i>
+                  Chỉnh sửa
+              </a>
+              |
+              <a href="javascript:void(0)" onclick="confirmDel(${user.id})" class="text-delete" data-toggle="modal" data-target="#exampleModal">
+  
+                  <i class="fa fa-trash-alt">
+                  </i>
+                  Xoá
+              </a>
+          </td>
+      </tr>`;
+      }
+      $("tbody").html(htmlString);
+    });
+
+    $.ajax({
+      method: "GET",
+      url: "https://vucodiing-students.herokuapp.com/users/?" + phoneFilter,
+      context: document.body,
+    }).done(function (users) {
+      let htmlString = "";
+
+      for (let user of users) {
+        htmlString += `<tr id="${user.id}">
+          <td>${user.name}</td>
+          <td>${user.birthday}</td>
+          <td>${user.email}</td>
+          <td>${user.phone}</td>
+          <td>
+              <a href="repair-student.html?id=${user.id}" class="text-repair">
+                  <i class="fa fa-edit">
+                  </i>
+                  Chỉnh sửa
+              </a>
+              |
+              <a href="javascript:void(0)" onclick="confirmDel(${user.id})" class="text-delete" data-toggle="modal" data-target="#exampleModal">
+  
+                  <i class="fa fa-trash-alt">
+                  </i>
+                  Xoá
+              </a>
+          </td>
+      </tr>`;
+      }
+      $("tbody").html(htmlString);
+    });
+  }
 }
 
 function confirmDel(id) {
@@ -188,5 +293,4 @@ function dellStudent() {
       $("#exampleModal").modal("hide");
     });
   }
-  
 }
