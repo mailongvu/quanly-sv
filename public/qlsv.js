0a776d88c509c13082f361ba8a0cce1b2e26c477
VuCodiing <vucodiing@VuCodiings-iMac.local>
2020-09-08 15:30:46 +0700

    make pages quanly-sv

--- public/qlsv.js
+++ public/qlsv.js
@@ -4,7 +4,8 @@ $(function () {
 
   $.ajax({
     type: "GET",
-    url: http,
+    url: http + "?_sort=id &_order=desc &_page=1",
+    
     context: document.body
   }).done(function (users) {
     let htmlString = "";
@@ -35,6 +36,142 @@ $(function () {
   });
 
 })
+
+function btn1() {
+  $.ajax({
+    type: "GET",
+    url: http + "?_sort=id &_order=desc &_page=1",
+    context: document.body
+  }).done(function (users) {
+    let htmlString = "";
+
+    for (let user of users) {
+      htmlString += `<tr id="${user.id}">
+        <td>${user.name}</td>
+        <td>${user.birthday}</td>
+        <td>${user.email}</td>
+        <td>${user.phone}</td>
+        <td>
+            <a href="repair-student.html?id=${user.id}" class="text-repair">
+                <i class="fa fa-edit">
+                </i>
+                Chỉnh sửa
+            </a>
+            |
+            <a href="javascript:void(0)" onclick="confirmDel(${user.id})" class="text-delete" data-toggle="modal" data-target="#exampleModal">
+
+                <i class="fa fa-trash-alt">
+                </i>
+                Xoá
+            </a>
+        </td>
+    </tr>`
+    }
+    $("tbody").html(htmlString);
+  });
+}
+
+function btn2() {
+  $.ajax({
+    type: "GET",
+    url: http + "?_sort=id &_order=desc &_page=2",
+    context: document.body
+  }).done(function (users) {
+    let htmlString = "";
+
+    for (let user of users) {
+      htmlString += `<tr id="${user.id}">
+        <td>${user.name}</td>
+        <td>${user.birthday}</td>
+        <td>${user.email}</td>
+        <td>${user.phone}</td>
+        <td>
+            <a href="repair-student.html?id=${user.id}" class="text-repair">
+                <i class="fa fa-edit">
+                </i>
+                Chỉnh sửa
+            </a>
+            |
+            <a href="javascript:void(0)" onclick="confirmDel(${user.id})" class="text-delete" data-toggle="modal" data-target="#exampleModal">
+
+                <i class="fa fa-trash-alt">
+                </i>
+                Xoá
+            </a>
+        </td>
+    </tr>`
+    }
+    $("tbody").html(htmlString);
+  });
+}
+
+function btn3() {
+  $.ajax({
+    type: "GET",
+    url: http + "?_sort=id &_order=desc &_page=3",
+    context: document.body
+  }).done(function (users) {
+    let htmlString = "";
+
+    for (let user of users) {
+      htmlString += `<tr id="${user.id}">
+        <td>${user.name}</td>
+        <td>${user.birthday}</td>
+        <td>${user.email}</td>
+        <td>${user.phone}</td>
+        <td>
+            <a href="repair-student.html?id=${user.id}" class="text-repair">
+                <i class="fa fa-edit">
+                </i>
+                Chỉnh sửa
+            </a>
+            |
+            <a href="javascript:void(0)" onclick="confirmDel(${user.id})" class="text-delete" data-toggle="modal" data-target="#exampleModal">
+
+                <i class="fa fa-trash-alt">
+                </i>
+                Xoá
+            </a>
+        </td>
+    </tr>`
+    }
+    $("tbody").html(htmlString);
+  });
+}
+
+function btn4() {
+  $.ajax({
+    type: "GET",
+    url: http + "?_sort=id &_order=desc &_page=4",
+    context: document.body
+  }).done(function (users) {
+    let htmlString = "";
+
+    for (let user of users) {
+      htmlString += `<tr id="${user.id}">
+        <td>${user.name}</td>
+        <td>${user.birthday}</td>
+        <td>${user.email}</td>
+        <td>${user.phone}</td>
+        <td>
+            <a href="repair-student.html?id=${user.id}" class="text-repair">
+                <i class="fa fa-edit">
+                </i>
+                Chỉnh sửa
+            </a>
+            |
+            <a href="javascript:void(0)" onclick="confirmDel(${user.id})" class="text-delete" data-toggle="modal" data-target="#exampleModal">
+
+                <i class="fa fa-trash-alt">
+                </i>
+                Xoá
+            </a>
+        </td>
+    </tr>`
+    }
+    $("tbody").html(htmlString);
+  });
+}
 
 function confirmDel(id) {
   studentID = id;
